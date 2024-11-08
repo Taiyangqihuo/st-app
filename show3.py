123a8997adbe65b9a6280c69a6040d2288ed0de8
unknown <5773507@qq.com>
2024-11-08 22:39:35 +0800

    CLTC Addtional

--- show3.py
+++ show3.py
@@ -67,132 +67,259 @@ st.divider()
 st.subheader("用户速度/加速度分布")
 if option!=None:
     show_on = st.toggle("显示散点",True)
-    df=pd.read_excel('wltc.xlsx',header=None)
-    df.columns = ['t','v']
-    v=df['v'].values
-    a=np.diff(v)/3.6
-    a=np.append(a,0)
-    idx=(v!= 0) | (a!= 0)
-    v0=v[idx]
-    a0=a[idx]
-    trace1 = {
-    "type": "histogram2d", 
-    "x": v0,
-    "y": a0, 
-    "xbins": {
-        "end": 140, 
-        "size": 5, 
-        "start": 0
-    }, 
-    "ybins": {
-        "end": 2, 
-        "size": 0.1, 
-        "start": -2
-    }, 
-    "zsmooth": "best", 
-    "colorbar": {
-        "x": 0.99, 
-        "y": 0.35, 
-        "len": 0.7, 
-        "ticks": "outside", 
-        "title": "加速度分布", 
-        "thickness": 20, 
-        "titleside": "right"
-    }, 
-    "colorscale": "Portland"
-    #   "colorscale": [[0, 'rgb(12,51,131)'], [0.02, 'rgb(10,136,186)'], [0.04, 'rgb(242,211,56)'], [0.08, 'rgb(242,143,56)'], [1, 'rgb(217,30,30)']]
-    }
-    trace2 = {
-    "mode": "markers", 
-    "name": "", 
-    "type": "scatter", 
-    "x": v0,
-    "y": a0,
-    "marker": {
-        "size": 2, 
-        "color": "#e0e0e0"
-    }, 
-    #   "text": [0:1:1800]
-    }
-    trace3 = {
-    "name": "", 
-    "type": "histogram", 
-    "x":v0,
-    "xaxis": "x1", 
-    "xbins": {
-        "end": 140, 
-        "size": 5, 
-        "start": 0
-    }, 
-    "yaxis": "y2", 
-    "marker": {"color": "rgb(242,211,56)"}
-    }
-    trace4 = {
-    "name": "", 
-    "type": "histogram", 
-    "y": a0,
-    "xaxis": "x2", 
-    "yaxis": "y1", 
-    "ybins": {
-        "end": 2, 
-        "size": 0.1, 
-        "start": -2
-    }, 
-    "marker": {"color": "rgb(242,211,56)"}
-    }
-    if show_on==True:
-        data = Data([trace1, trace2, trace3, trace4])
-    else:
-        data = Data([trace1, trace3, trace4])
-    layout = {
-    "font": {
-        "size": 13, 
-        "family": "PT Sans Narrow, sans-serif"
-    }, 
-    "title": "速度分布", 
-    "width": 650, 
-    "xaxis": {
-        "range": [0, 150], 
-        "title": "v", 
-        "domain": [0, 0.7], 
-        "zeroline": False
-    }, 
-    "yaxis": {
-        "range": [-2, 2], 
-        "title": "a", 
-        "domain": [0, 0.7], 
-        "showgrid": False, 
-        "zeroline": False
-    }, 
-    "height": 520, 
-    "xaxis2": {
-        "domain": [0.75, 1], 
-        "showgrid": True, 
-        "zeroline": False
-    }, 
-    "yaxis2": {
-        "domain": [0.75, 1], 
-        "showgrid": True, 
-        "zeroline": False
-    }, 
-    "autosize": False, 
-    "showlegend": False, 
-    "annotations": [
-        {
-        "x": 1, 
-        "y": 1, 
-        "text": "WLTC循环工况", 
-        "xref": "paper", 
-        "yref": "paper", 
-        "xanchor": "right", 
-        "yanchor": "bottom", 
-        "showarrow": False
+    t1,t2=st.tabs(["WLTC","CLTC"])
+    with t1:
+        df=pd.read_excel('wltc.xlsx',header=None)
+        df.columns = ['t','v']
+        v=df['v'].values
+        a=np.diff(v)/3.6
+        a=np.append(a,0)
+        idx=(v!= 0) | (a!= 0)
+        v0=v[idx]
+        a0=a[idx]
+        trace1 = {
+        "type": "histogram2d", 
+        "x": v0,
+        "y": a0, 
+        "xbins": {
+            "end": 140, 
+            "size": 5, 
+            "start": 0
+        }, 
+        "ybins": {
+            "end": 2, 
+            "size": 0.1, 
+            "start": -2
+        }, 
+        "zsmooth": "best", 
+        "colorbar": {
+            "x": 0.99, 
+            "y": 0.35, 
+            "len": 0.7, 
+            "ticks": "outside", 
+            "title": "加速度分布", 
+            "thickness": 20, 
+            "titleside": "right"
+        }, 
+        "colorscale": "Portland"
+        #   "colorscale": [[0, 'rgb(12,51,131)'], [0.02, 'rgb(10,136,186)'], [0.04, 'rgb(242,211,56)'], [0.08, 'rgb(242,143,56)'], [1, 'rgb(217,30,30)']]
         }
-    ]
-    }
-    fig = Figure(data=data, layout=layout)
-    st.plotly_chart(fig, use_container_width=True)
-
+        trace2 = {
+        "mode": "markers", 
+        "name": "", 
+        "type": "scatter", 
+        "x": v0,
+        "y": a0,
+        "marker": {
+            "size": 2, 
+            "color": "#e0e0e0"
+        }, 
+        #   "text": [0:1:1800]
+        }
+        trace3 = {
+        "name": "", 
+        "type": "histogram", 
+        "x":v0,
+        "xaxis": "x1", 
+        "xbins": {
+            "end": 140, 
+            "size": 5, 
+            "start": 0
+        }, 
+        "yaxis": "y2", 
+        "marker": {"color": "rgb(242,211,56)"}
+        }
+        trace4 = {
+        "name": "", 
+        "type": "histogram", 
+        "y": a0,
+        "xaxis": "x2", 
+        "yaxis": "y1", 
+        "ybins": {
+            "end": 2, 
+            "size": 0.1, 
+            "start": -2
+        }, 
+        "marker": {"color": "rgb(242,211,56)"}
+        }
+        if show_on==True:
+            data = Data([trace1, trace2, trace3, trace4])
+        else:
+            data = Data([trace1, trace3, trace4])
+        layout = {
+        "font": {
+            "size": 13, 
+            "family": "PT Sans Narrow, sans-serif"
+        }, 
+        "title": "速度分布", 
+        "width": 650, 
+        "xaxis": {
+            "range": [0, 150], 
+            "title": "v", 
+            "domain": [0, 0.7], 
+            "zeroline": False
+        }, 
+        "yaxis": {
+            "range": [-2, 2], 
+            "title": "a", 
+            "domain": [0, 0.7], 
+            "showgrid": False, 
+            "zeroline": False
+        }, 
+        "height": 520, 
+        "xaxis2": {
+            "domain": [0.75, 1], 
+            "showgrid": True, 
+            "zeroline": False
+        }, 
+        "yaxis2": {
+            "domain": [0.75, 1], 
+            "showgrid": True, 
+            "zeroline": False
+        }, 
+        "autosize": False, 
+        "showlegend": False, 
+        "annotations": [
+            {
+            "x": 1, 
+            "y": 1, 
+            "text": "WLTC循环工况", 
+            "xref": "paper", 
+            "yref": "paper", 
+            "xanchor": "right", 
+            "yanchor": "bottom", 
+            "showarrow": False
+            }
+        ]
+        }
+        fig = Figure(data=data, layout=layout)
+        st.plotly_chart(fig, use_container_width=True)
+    with t2:
+        df=pd.read_excel('cltc.xlsx',header=None)
+        df.columns = ['t','v']
+        v=df['v'].values
+        a=np.diff(v)/3.6
+        a=np.append(a,0)
+        idx=(v!= 0) | (a!= 0)
+        v0=v[idx]
+        a0=a[idx]
+        trace1 = {
+        "type": "histogram2d", 
+        "x": v0,
+        "y": a0, 
+        "xbins": {
+            "end": 140, 
+            "size": 5, 
+            "start": 0
+        }, 
+        "ybins": {
+            "end": 2, 
+            "size": 0.1, 
+            "start": -2
+        }, 
+        "zsmooth": "best", 
+        "colorbar": {
+            "x": 0.99, 
+            "y": 0.35, 
+            "len": 0.7, 
+            "ticks": "outside", 
+            "title": "加速度分布", 
+            "thickness": 20, 
+            "titleside": "right"
+        }, 
+        "colorscale": "Portland"
+        #   "colorscale": [[0, 'rgb(12,51,131)'], [0.02, 'rgb(10,136,186)'], [0.04, 'rgb(242,211,56)'], [0.08, 'rgb(242,143,56)'], [1, 'rgb(217,30,30)']]
+        }
+        trace2 = {
+        "mode": "markers", 
+        "name": "", 
+        "type": "scatter", 
+        "x": v0,
+        "y": a0,
+        "marker": {
+            "size": 2, 
+            "color": "#e0e0e0"
+        }, 
+        #   "text": [0:1:1800]
+        }
+        trace3 = {
+        "name": "", 
+        "type": "histogram", 
+        "x":v0,
+        "xaxis": "x1", 
+        "xbins": {
+            "end": 140, 
+            "size": 5, 
+            "start": 0
+        }, 
+        "yaxis": "y2", 
+        "marker": {"color": "rgb(242,211,56)"}
+        }
+        trace4 = {
+        "name": "", 
+        "type": "histogram", 
+        "y": a0,
+        "xaxis": "x2", 
+        "yaxis": "y1", 
+        "ybins": {
+            "end": 2, 
+            "size": 0.1, 
+            "start": -2
+        }, 
+        "marker": {"color": "rgb(242,211,56)"}
+        }
+        if show_on==True:
+            data = Data([trace1, trace2, trace3, trace4])
+        else:
+            data = Data([trace1, trace3, trace4])
+        layout = {
+        "font": {
+            "size": 13, 
+            "family": "PT Sans Narrow, sans-serif"
+        }, 
+        "title": "速度分布", 
+        "width": 650, 
+        "xaxis": {
+            "range": [0, 150], 
+            "title": "v", 
+            "domain": [0, 0.7], 
+            "zeroline": False
+        }, 
+        "yaxis": {
+            "range": [-2, 2], 
+            "title": "a", 
+            "domain": [0, 0.7], 
+            "showgrid": False, 
+            "zeroline": False
+        }, 
+        "height": 520, 
+        "xaxis2": {
+            "domain": [0.75, 1], 
+            "showgrid": True, 
+            "zeroline": False
+        }, 
+        "yaxis2": {
+            "domain": [0.75, 1], 
+            "showgrid": True, 
+            "zeroline": False
+        }, 
+        "autosize": False, 
+        "showlegend": False, 
+        "annotations": [
+            {
+            "x": 1, 
+            "y": 1, 
+            "text": "CLTC循环工况", 
+            "xref": "paper", 
+            "yref": "paper", 
+            "xanchor": "right", 
+            "yanchor": "bottom", 
+            "showarrow": False
+            }
+        ]
+        }
+        fig = Figure(data=data, layout=layout)
+        st.plotly_chart(fig, use_container_width=True)
     #后面是用户工况
     df=pd.read_excel('user.xlsx',header=None)
     df.columns = ['t','v']
